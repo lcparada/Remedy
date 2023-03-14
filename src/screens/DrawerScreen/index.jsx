4637628316986810bde1229d95356a50493efe8f
lcparada <125614063+lcparada@users.noreply.github.com>
2023-03-14 15:41:01 -0300

    add:  new screen

--- src/screens/DrawerScreen/index.jsx
+++ src/screens/DrawerScreen/index.jsx
@@ -1,12 +1,9 @@
-import React from "react";
-import {Text} from "react-native"
-
-import styles from "./styles";
-
+import React from "react"
+import { Text, View } from "react-native"
 
 export default function DrawerScreen() {
-
-  return(
-    <Text>Ola</Text>
+  
+  return( 
+  <View><Text> ola</Text></View>
   )
 }
