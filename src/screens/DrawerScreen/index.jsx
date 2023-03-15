0d0c8af553992f9f988ff1b779d572f20f86d5b7
lcparada <125614063+lcparada@users.noreply.github.com>
2023-03-15 09:49:26 -0300

    add: new screen

--- src/screens/DrawerScreen/index.jsx
+++ src/screens/DrawerScreen/index.jsx
@@ -1,9 +1,148 @@
-import React from "react"
-import { Text, View } from "react-native"
+import React from "react";
+import { Text, View, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import { Feather } from "@expo/vector-icons";
+
+import styles from "./styles";
 
 export default function DrawerScreen() {
-  
-  return( 
-  <View><Text> ola</Text></View>
-  )
+  const [screen, setScreen] = React.useState(0);
+
+  const selectText = {
+    fontSize: 18,
+    fontFamily: "Lexend_600SemiBold",
+    color: "#68A6DA"
+  }
+
+  function handleScreenPrescription() {
+    if (screen !== 0) {
+      setScreen(0);
+    }
+  }
+
+  function handleScreenMedicine() {
+    if (screen !== 1) {
+      setScreen(1);
+    }
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.containerMainText}>
+        <Text style={styles.mainText}>Minha Gaveta</Text>
+      </View>
+
+      <View style={styles.containerButtons}>
+        <TouchableOpacity onPress={() => handleScreenPrescription()}>
+          <Text
+            style={screen === 0 ? selectText : styles.textButton }
+          >
+            {" "}
+            <Feather name="paperclip" size={24} color={screen === 0 ? "#68A6DA" : "black"} /> Receituários
+          </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => handleScreenMedicine()}>
+          <Text
+            style={screen === 1 ? selectText : styles.textButton }
+          >
+            {" "}
+            <Feather name="package" size={24} color={screen === 1 ? "#68A6DA" : "black"} /> Medicamentos
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      {screen === 0 ? (
+        <View title="prescriptionTotal">
+          <View style={styles.prescriptionScreen}>
+            <View>
+              <Text style={styles.prescriptionTextScreen}>
+                Infecções{"\n"}gastrointestinais
+              </Text>
+              <Text style={styles.prescriptionSubTextScreen}>
+                Criado em 30/12/2022
+              </Text>
+            </View>
+            <View style={styles.statusPrescription}>
+              <Text style={styles.statusPrescriptionTextProgress}>
+                Em andamento
+              </Text>
+              <Feather name="clock" size={18} color="#F6D664" />
+            </View>
+          </View>
+
+          <View style={styles.prescriptionScreen}>
+            <View>
+              <Text style={styles.prescriptionTextScreen}>Resfriado</Text>
+              <Text style={styles.prescriptionSubTextScreen}>
+                Criado em 20/12/2022
+              </Text>
+            </View>
+            <View style={styles.statusPrescription}>
+              <Text style={styles.statusPrescriptionTextFinished}>
+                Finalizado
+              </Text>
+              <Feather name="check-circle" size={18} color="#0B6E4F" />
+            </View>
+          </View>
+
+          <View style={styles.prescriptionScreen}>
+            <View>
+              <Text style={styles.prescriptionTextScreen}>Resfriado</Text>
+              <Text style={styles.prescriptionSubTextScreen}>
+                Criado em 20/12/2022
+              </Text>
+            </View>
+            <View style={styles.statusPrescription}>
+              <Text style={styles.statusPrescriptionTextAbandoned}>
+                Abandonado
+              </Text>
+              <Feather name="alert-circle" size={18} color="#721817" />
+            </View>
+          </View>
+        </View>
+      ) : (
+        <View title="medicineTotal">
+          <View style={styles.medicineScreen}>
+            <Text style={styles.medicineTextScreen}>Amoxilina</Text>
+
+            <View style={styles.medicineStatus}>
+              <Feather name="check-circle" size={18} color="#0B6E4F" />
+              <Text style={styles.medicineStatusTextScreenEnough}>
+                Suficiente
+              </Text>
+            </View>
+
+            <Text style={styles.medicineTextStatus}>
+              • Você contêm 30 comprimidos.
+            </Text>
+
+            <Text style={styles.medicineTextStatus}>
+              • Você precisa de 20 comprimidos para concluir seus tratamentos
+            </Text>
+          </View>
+
+          <View style={styles.medicineScreen}>
+            <Text style={styles.medicineTextScreen}>Amoxilina</Text>
+
+            <View style={styles.medicineStatus}>
+              <Feather name="alert-circle" size={18} color="#721817" />
+              <Text style={styles.medicineStatusTextScreenInsufficient}>
+                Insuficiente
+              </Text>
+            </View>
+
+            <Text style={styles.medicineTextStatus}>
+              • Você contêm 30 comprimidos.
+            </Text>
+
+            <Text style={styles.medicineTextStatus}>
+              • Você precisa de 20 comprimidos para concluir seus tratamentos
+            </Text>
+          </View>
+        </View>
+      )}
+    </SafeAreaView>
+  );
 }
--- src/screens/DrawerScreen/styles.js
+++ src/screens/DrawerScreen/styles.js
@@ -1,8 +1,108 @@
 import { StyleSheet } from "react-native";
 
-const styles = StyleSheet.create({
-  
-})
+import { NavigationContainer } from "@react-navigation/native";
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  containerMainText: {
+    marginLeft: 30,
+    marginTop: 78,
+  },
+  mainText: {
+    fontSize: 28,
+    fontFamily: "Lexend_600SemiBold",
+  },
+  containerButtons: {
+    flexDirection: "row",
+    marginTop: 32,
+    marginLeft: 20,
+    columnGap: 23,
+  },
+  textButton: {
+    fontSize: 18,
+    fontFamily: "Lexend_400Regular",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  selectTextButton : {
+    fontSize: 18,
+    fontFamily: "Lexend_600SemiBold",
+    alignItems: "center",
+    justifyContent: "center",
+    color: "#68A6DA"
+
+  },
+  prescriptionScreen: {
+    marginLeft: 30,
+    marginTop: 20,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  prescriptionTextScreen: {
+    fontSize: 18,
+    fontFamily: "Lexend_600SemiBold",
+    color: "#68A6DA"
+  },
+  prescriptionSubTextScreen: {
+    fontSize: 14,
+    fontFamily: "Lexend_400Regular"
+  },
+  statusPrescription: {
+    marginRight: 32,
+    flexDirection: "row",
+    columnGap: 10
+  },
+  statusPrescriptionTextProgress: {
+    fontSize: 13,
+    fontFamily:"Lexend_600SemiBold",
+    color: "#F6D664",
+    
+
+  },
+  statusPrescriptionTextFinished: {
+    fontSize: 13,
+    fontFamily: "Lexend_600SemiBold",
+    color: "#0B6E4F"
+  },
+  statusPrescriptionTextAbandoned: {
+    fontSize: 13,
+    fontFamily: "Lexend_600SemiBold",
+    color: "#721817"
+  },
+  medicineScreen:{
+    marginLeft: 30,
+    marginTop: 22
+  },
+  medicineTextScreen: {
+    fontSize: 18,
+    fontFamily: "Lexend_600SemiBold",
+    color: "#68A6DA"
+  },
+  medicineStatus: {
+    flexDirection: "row",
+    columnGap: 8,
+    marginTop: 8
+  },
+  medicineStatusTextScreenEnough: {
+    fontSize: 12,
+    fontFamily: "Lexend_400Regular",
+    color: "#0B6E4F"
+  },
+  medicineStatusTextScreenInsufficient: {
+    fontSize: 12,
+    fontFamily: "Lexend_400Regular",
+    color: "#721817"
+  },
+  medicineTextStatus: {
+    fontSize: 14,
+    fontFamily: "Lexend_400Regular",
+    marginTop: 5,
+    width: 260
+  }
+
+});
 
 export default styles;
