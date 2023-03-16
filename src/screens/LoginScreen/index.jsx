3efdd9b9d6ad101468add7ca1c8c706930f423db
lcparada <125614063+lcparada@users.noreply.github.com>
2023-03-16 15:40:31 -0300

    add: loginScreen

---
+++ src/screens/LoginScreen/index.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import {
+  View,
+  SafeAreaView,
+  TextInput,
+  TouchableOpacity,
+  Text,
+  Keyboard,
+  Pressable,
+} from "react-native";
+
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
+
+import styles from "./styles";
+
+export default function LoginScreen() {
+  const [showPasswordOrNot, setShowPasswordOrNot] =
+    React.useState("eye-off-outline");
+  const [showDigits, setshowDigits] = React.useState(true);
+
+  function showPassword() {
+    if (showPasswordOrNot === "eye-off-outline") {
+      setShowPasswordOrNot("eye-outline");
+      setshowDigits(false);
+    } else {
+      setShowPasswordOrNot("eye-off-outline");
+      setshowDigits(true);
+    }
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Pressable onPress={() => Keyboard.dismiss()}>
+        <View style={styles.logo}>
+          <MaterialCommunityIcons
+            name="pill"
+            size={77}
+            color="#68A6DA"
+            style={{ marginBottom: 60 }}
+          />
+        </View>
+
+        <View style={styles.containerInput}>
+          <View style={styles.inputUser}>
+            <TextInput style={styles.input} placeholder="Usuário"></TextInput>
+            <Ionicons name="person-outline" size={24} color="#929292" />
+          </View>
+
+          <View style={styles.inputUser}>
+            <TextInput
+              style={styles.input}
+              placeholder="Senha"
+              secureTextEntry={showDigits}
+            ></TextInput>
+            <TouchableOpacity onPress={() => showPassword()}>
+              <Ionicons name={showPasswordOrNot} size={24} color="#929292" />
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>ENTRAR</Text>
+        </TouchableOpacity>
+
+        <View style={styles.forgotPassword}>
+          <TouchableOpacity>
+            <Text style={styles.textForgotPassword}>Esqueceu a senha?</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.register}>
+          <Text style={styles.subTextRegister}>Ainda não tem conta?</Text>
+          <TouchableOpacity>
+            <Text style={styles.textRegister}>Registre-se!</Text>
+          </TouchableOpacity>
+        </View>
+      </Pressable>
+    </SafeAreaView>
+  );
+}
---
+++ src/screens/LoginScreen/styles.js
@@ -0,0 +1,73 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  logo: {
+    alignItems: "center",
+  },
+  input: {
+    width: 265,
+    height: 40,
+  },
+  containerInput: {
+    flexDirection: "column",
+    rowGap: 20,
+  },
+  inputUser: {
+    flexDirection: "row",
+    alignItems: "center",
+    width: 330,
+    height: 60,
+    borderRadius: 10,
+    backgroundColor: "#F5F7F9",
+    paddingLeft: 20,
+    justifyContent: "space-between",
+    paddingRight: 20,
+    
+  },
+  button: {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#68A6DA",
+    width: 330,
+    height: 60,
+    marginTop: 30,
+    borderRadius: 30,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+    fontFamily: "Lexend_700Bold",
+  },
+  forgotPassword: {
+    marginTop: 40,
+    alignItems: "center"
+  },
+  textForgotPassword: {
+    color: "#68A6DA",
+    fontSize: 16,
+    fontFamily: "Lexend_700Bold",
+  },
+  register: {
+    alignItems: "center",
+    justifyContent: "center",
+    top: 175
+  },
+  textRegister: {
+    color: "#68A6DA",
+    fontSize: 16,
+    fontFamily: "Lexend_700Bold",
+    alignItems: "center"
+  },
+  subTextRegister: {
+    alignItems: "center",
+    fontSize: 14,
+    fontFamily: "Lexend_300Light"
+  }
+});
+
+export default styles;
