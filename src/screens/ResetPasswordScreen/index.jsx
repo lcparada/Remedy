f497281a43c2f377467a2bfaa2436b3c49cdd3c0
lcparada <125614063+lcparada@users.noreply.github.com>
2023-03-17 18:07:58 -0300

    add: reset password screen

---
+++ src/screens/ResetPasswordScreen/index.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import {
+    View,
+    Text,
+    TextInput,
+    SafeAreaView,
+    Pressable,
+    StatusBar,
+    Keyboard,
+    TouchableOpacity
+} from "react-native";
+
+import styles from "./styles";
+
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+export default function ResetPasswordScreen() {
+    return (
+        <SafeAreaView style={styles.container} >
+            <StatusBar style="auto" />
+            <Pressable style={styles.containerPressable} onPress={() => Keyboard.dismiss()}>
+
+                <View style={styles.containerMainText}>
+                    <Text style={styles.mainText}>Resetar senha</Text>
+                </View>
+
+                <View style={styles.containerSubText}>
+                    <Text style={styles.subText}>Entre com o email associado a sua conta e {"\n"}nós
+                        vamos enviar um email com instruções{"\n"}para
+                        redefinir sua senha.</Text>
+                </View>
+
+                <View style={styles.containerInputEmail}>
+                    <TextInput style={styles.inputEmail} placeholder="Seu email aqui!"></TextInput>
+                    <MaterialCommunityIcons name="email-outline" size={24} color="#929292" />
+                </View>
+
+                <View style={styles.containerButton}>
+                    <TouchableOpacity style={styles.button}><Text style={styles.textButton}>Enviar</Text></TouchableOpacity>
+                </View>
+                
+                <View style={styles.containerLogo}>
+                <MaterialCommunityIcons
+                    name="pill"
+                    size={77}
+                    color="#68A6DA"
+                    style={{opacity: 0.5}}
+                />
+                </View>
+
+            </Pressable>
+        </SafeAreaView>
+    );
+}
---
+++ src/screens/ResetPasswordScreen/styles.js
@@ -0,0 +1,70 @@
+import { StyleSheet } from "react-native";
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    containerPressable: {
+        flex: 1,
+    },
+    containerMainText: {
+        marginTop: 144,
+        marginLeft: 30
+    },
+    mainText: {
+        fontSize: 35,
+        fontFamily: "Lexend_700Bold",
+    },
+    containerSubText: {
+        marginTop: 15,
+        marginLeft: 30
+    },
+    subText: {
+        fontSize: 15,
+        fontFamily: "Lexend_300Light",
+        color: "#99A0A8"
+    },
+    containerInputEmail: {
+        marginTop: 45,
+        marginLeft: 30,
+        height: 60,
+        width: 330,
+        borderRadius: 10,
+        backgroundColor: "#F5F7F9",
+        flexDirection: "row",
+        alignItems: "center"
+    },
+    inputEmail: {
+        fontSize: 16,
+        fontFamily: "Lexend_400Regular",
+        paddingLeft: 20,
+        width: 290
+    },
+    containerButton: {
+        marginTop: 15,
+        marginLeft: 30,
+    },
+    button: {
+        width: 330,
+        height: 60,
+        borderRadius: 30,
+        backgroundColor: "#68A6DA",
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    textButton: {
+        fontSize: 16,
+        fontFamily: "Lexend_700Bold",
+        color: "white"
+    },
+    containerLogo: {
+    width: 330,
+    marginLeft: 30,
+    alignItems:"center",
+    marginTop: 185
+    },
+    
+})
+
+export default styles
