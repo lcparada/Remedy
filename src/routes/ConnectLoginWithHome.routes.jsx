92c54ec3bf837eb26f553c0b3ca3e87cdf85a835
lcparada <125614063+lcparada@users.noreply.github.com>
2023-03-19 21:35:48 -0300

    add: navigation between login and app

---
+++ src/routes/ConnectLoginWithHome.routes.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
+
+import AppRoutes from "./app.routes";
+import LoginScreenRoutes from "./LoginScreen.routes";
+import { NavigationContainer } from "@react-navigation/native";
+
+export default function ConnectLoginWithHome() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="LoginScreenRoutes" component={LoginScreenRoutes} options={{
+                    headerShown: false
+                }} />
+                <Stack.Screen name="AppRoutes" component={AppRoutes} options={{
+                    headerShown: false
+                }} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
